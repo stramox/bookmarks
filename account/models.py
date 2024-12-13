f451df5ca5556838fc7a569faeaa6e7bbb188117
nariman <nariman.h98@gmail.com>
2024-12-13 16:29:21 +0330

    Initial commit

---
+++ account/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.conf import settings
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f"profile of {self.user.username}"
+
+
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_from_set', on_delete=models.CASCADE
+    )
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_to_set', on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=["-created"]),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+# Add the following field to User Dynamically
+user_model = get_user_model()
+user_model.add_to_class(
+    'following', models.ManyToManyField(
+        'self', through=Contact,
+        related_name='followers',
+        symmetrical=False
+    )
+)
